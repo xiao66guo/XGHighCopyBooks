b3c6a1f152898eb9a2f22808422eb1a10ba63308
一个老兵 <xiao66guo@163.com>
2017-01-04 14:34:27 +0800

    Initial Commit

---
+++ XGHighCopyBooks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XGHighCopyBooks
+//
+//  Created by 小果 on 2017/1/4.
+//  Copyright © 2017年 小果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XGHighCopyBooks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XGHighCopyBooks
+//
+//  Created by 小果 on 2017/1/4.
+//  Copyright © 2017年 小果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
